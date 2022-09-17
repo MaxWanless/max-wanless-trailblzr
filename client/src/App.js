48c37c51b17b2330e8a72db3151b4d934ed97a61
Max Wanless <maxwanless@gmail.com>
2022-09-17 15:07:44 -0400

    Created routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Account from "./pages/Account/Account";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Favourites from "./pages/Favourites/Favourites";
+import ParkDetails from "./pages/ParkDetails/ParkDetails";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
 import "./App.scss";
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/:parkId" element={<ParkDetails />} />
+          <Route path="/favourites" element={<Favourites />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
