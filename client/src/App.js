2cea50c4c87844279138737cbf8529c7d7186877
Max Wanless <maxwanless@gmail.com>
2022-09-17 16:39:59 -0400

    Created header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Account from "./pages/Account/Account";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Favourites from "./pages/Favourites/Favourites";
 import ParkDetails from "./pages/ParkDetails/ParkDetails";
 import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/SignUp/SignUp";
+import Header from "./components/Header/Header";
 import "./App.scss";
 
 function App() {
+  const theme = createTheme({
+    palette: {},
+  });
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/dashboard" />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/dashboard/:parkId" element={<ParkDetails />} />
-          <Route path="/favourites" element={<Favourites />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/dashboard/:parkId" element={<ParkDetails />} />
+            <Route path="/favourites" element={<Favourites />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,161 @@
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Container,
+  Box,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+  Button,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import TerrainIcon from "@mui/icons-material/Terrain";
+import SearchIcon from "@mui/icons-material/Search";
+import "./Header.scss";
+
+const Header = () => {
+  const pages = ["Dashboard", "Favourites", "Account"];
+  const [showNavMenu, setShowNavMenu] = useState(false);
+  const handleOpenNavMenu = (event) => {
+    setShowNavMenu(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setShowNavMenu(null);
+  };
+  return (
+    <AppBar
+      position="static"
+      sx={{ background: "transparent", boxShadow: "none" }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar>
+          {/* Desktop Logo */}
+          <Box
+            sx={{ display: { xs: "none", md: "flex" }, alignItems: "center" }}
+          >
+            <TerrainIcon sx={{ mr: 1 }} />
+            <Typography
+              variant="h4"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              TrailBLZR
+            </Typography>
+          </Box>
+          {/* Desktop Menu */}
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page, index) => (
+              <Button
+                className="header__nav-button"
+                component={NavLink}
+                to={`/${page}`}
+                key={page}
+                sx={{ color: "#ffffff" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          {/* Mobile Menu */}
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={showNavMenu}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(showNavMenu)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page, index) => (
+                <Link className="header__link" key={index} to={`/${page}`}>
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                </Link>
+              ))}
+            </Menu>
+          </Box>
+          {/* Mobile Logo */}
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
+              alignItems: "center",
+            }}
+          >
+            <TerrainIcon sx={{ mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              TrailBLZR
+            </Typography>
+          </Box>
+          {/* Mobile Search */}
+          <Box
+            sx={{
+              flexGrow: 0,
+              display: { xs: "flex", md: "none" },
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={null}
+              color="inherit"
+            >
+              <SearchIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
