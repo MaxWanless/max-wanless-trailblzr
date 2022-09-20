7e23f9d764555213a90e3b1e9a095f162fbd9017
Max Wanless <maxwanless@gmail.com>
2022-09-20 15:44:58 -0400

    Added user Signup and Signing calls

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import CardContent from "@mui/material/CardContent";
 import TextField from "@mui/material/TextField";
@@ -9,45 +9,66 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Checkbox from "@mui/material/Checkbox";
 import "./SignIn.scss";
+import axios from "axios";
 
 function SignIn() {
+  const handleChange = (name) => (event) => {};
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let userInfo = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+    axios
+      .post("http://localhost:5050/user/register", userInfo)
+      .then((response) => {})
+      .catch((error) => {});
+  };
+
   return (
     <Container
-      maxWidth="lg"
+      maxWidth="xs"
       sx={{
         display: "flex",
         flexDirection: "column",
       }}
     >
-      <Card>
+      <Card sx={{ marginTop: "10vh" }}>
         <CardContent>
           <Typography variant="h2">Sign In</Typography>
-          <FormGroup>
+          <Box component="form" onSubmit={handleSubmit}>
             <TextField
               label="Username"
+              name="username"
               variant="outlined"
               required
               size="small"
               margin="normal"
               fullWidth
+              autoComplete="username"
+              onChange={handleChange("userName")}
             />
             <TextField
               label="Password"
+              name="password"
               variant="outlined"
               required
               size="small"
               margin="normal"
               fullWidth
               type="password"
+              autoComplete="current-password"
+              onChange={handleChange("password")}
             />
             <FormControlLabel
               control={<Checkbox />}
               label="Remember Me"
             ></FormControlLabel>
-            <Button variant="contained" fullWidth>
+            <Button variant="contained" fullWidth type="submit">
               Sign In
             </Button>
-          </FormGroup>
+          </Box>
           <Typography variant="body2" marginTop={"1rem"}>
             New to TrailBLZR? <Link to="/signup">Sign up now.</Link>
           </Typography>
--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import axios from "axios";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import FormGroup from "@mui/material/FormGroup";
 import CardContent from "@mui/material/CardContent";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
@@ -9,6 +11,27 @@ import Button from "@mui/material/Button";
 import "./SignUp.scss";
 
 function SignUp() {
+  const [registerSuccess, setRegisterSuccess] = useState(false);
+  const [userExists, setUserExits] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let newUser = {
+      firstName: event.target.firstname.value,
+      lastName: event.target.lastname.value,
+      email: event.target.email.value,
+      userName: event.target.username.value,
+      password: event.target.password.value,
+    };
+    axios
+      .post("http://localhost:5050/user/register", newUser)
+      .then((response) => setRegisterSuccess(true))
+      .catch((error) => setUserExits(true));
+  };
+
+  if (registerSuccess) {
+    return <Navigate to="/" />;
+  }
   return (
     <Container
       maxWidth="xs"
@@ -17,12 +40,13 @@ function SignUp() {
         flexDirection: "column",
       }}
     >
-      <Card>
+      <Card sx={{ marginTop: "10vh" }}>
         <CardContent>
           <Typography variant="h2">Sign Up</Typography>
-          <FormGroup>
+          <Box component="form" onSubmit={handleSubmit}>
             <TextField
               label="First Name"
+              name="firstname"
               variant="outlined"
               required
               size="small"
@@ -31,6 +55,7 @@ function SignUp() {
             />
             <TextField
               label="Last Name"
+              name="lastname"
               variant="outlined"
               required
               size="small"
@@ -39,33 +64,39 @@ function SignUp() {
             />
             <TextField
               label="Email"
+              name="email"
               variant="outlined"
               required
               size="small"
               margin="normal"
               fullWidth
+              autoComplete="email"
             />
             <TextField
               label="Username"
+              name="username"
               variant="outlined"
               required
               size="small"
               margin="normal"
               fullWidth
+              autoComplete="username"
             />
             <TextField
               label="Password"
+              name="password"
               variant="outlined"
               required
               size="small"
               margin="normal"
               fullWidth
               type="password"
+              autoComplete="current-password"
             />
             <Button type="submit" variant="contained" fullWidth>
               Sign Up
             </Button>
-          </FormGroup>
+          </Box>
           <Typography variant="body2" marginTop={"1rem"}>
             Already a member? <Link to="/signin">Sign In</Link>
           </Typography>
