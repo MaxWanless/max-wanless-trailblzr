a81632d4d5468fce6eda32bca808da1a73865e23
Max Wanless <maxwanless@gmail.com>
2022-09-20 11:28:36 -0400

    Added Sigin and Signup pages

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -1,8 +1,60 @@
-import { Container } from "@mui/material";
+import { Link } from "react-router-dom";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import CardContent from "@mui/material/CardContent";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
 import "./SignIn.scss";
 
 function SignIn() {
-  return <Container maxWidth="lg">Sign In</Container>;
+  return (
+    <Container
+      maxWidth="lg"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Card>
+        <CardContent>
+          <Typography variant="h2">Sign In</Typography>
+          <FormGroup>
+            <TextField
+              label="Username"
+              variant="outlined"
+              required
+              size="small"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              label="Password"
+              variant="outlined"
+              required
+              size="small"
+              margin="normal"
+              fullWidth
+              type="password"
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Remember Me"
+            ></FormControlLabel>
+            <Button variant="contained" fullWidth>
+              Sign In
+            </Button>
+          </FormGroup>
+          <Typography variant="body2" marginTop={"1rem"}>
+            New to TrailBLZR? <Link to="/signup">Sign up now.</Link>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 }
 
 export default SignIn;
--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,78 @@
-import { Container } from "@mui/material";
+import { Link } from "react-router-dom";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import FormGroup from "@mui/material/FormGroup";
+import CardContent from "@mui/material/CardContent";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import "./SignUp.scss";
 
 function SignUp() {
-  return <Container maxWidth="lg">Sign Up</Container>;
+  return (
+    <Container
+      maxWidth="xs"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Card>
+        <CardContent>
+          <Typography variant="h2">Sign Up</Typography>
+          <FormGroup>
+            <TextField
+              label="First Name"
+              variant="outlined"
+              required
+              size="small"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              label="Last Name"
+              variant="outlined"
+              required
+              size="small"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              label="Email"
+              variant="outlined"
+              required
+              size="small"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              label="Username"
+              variant="outlined"
+              required
+              size="small"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              label="Password"
+              variant="outlined"
+              required
+              size="small"
+              margin="normal"
+              fullWidth
+              type="password"
+            />
+            <Button type="submit" variant="contained" fullWidth>
+              Sign Up
+            </Button>
+          </FormGroup>
+          <Typography variant="body2" marginTop={"1rem"}>
+            Already a member? <Link to="/signin">Sign In</Link>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 }
 
 export default SignUp;
