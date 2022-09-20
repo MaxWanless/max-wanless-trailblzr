4d3c1e0f39c81bf315a5d0e9d4b1132bd9102c9d
Max Wanless <maxwanless@gmail.com>
2022-09-20 16:52:44 -0400

    Added Register and Login controllers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,88 @@
-exports.userList = (req, res) => {};
+const knex = require("knex")(require("../knexfile"));
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+exports.userList = (req, res) => {
+  knex("users").then((data) => {
+    res.status(200).json(data);
+  });
+};
+
+exports.userDetails = (req, res) => {};
+
+exports.register = (req, res) => {
+  const { firstName, lastName, userName, email, password } = req.body;
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
+  knex
+    .select("userName")
+    .from("users")
+    .where("userName", userName)
+    .then((data) => {
+      if (!data.length) {
+        return knex("users")
+          .insert({
+            firstName: firstName,
+            lastName: lastName,
+            userName: userName,
+            email: email,
+            password: hashedPassword,
+          })
+          .then((data) => {
+            res
+              .status(201)
+              .json({
+                message: "user signed up successfully",
+                data,
+              })
+              .catch((error) => {
+                res
+                  .status(400)
+                  .json({ message: "Issue signing up user", error: error });
+              });
+          });
+      } else {
+        res
+          .status(400)
+          .json({ message: "User with that username already exists" });
+      }
+    })
+    .catch((error) => {
+      res.status(400).json({
+        message: "Ran into issue signing up user",
+        erorr: error,
+      });
+    });
+};
+
+exports.login = (req, res) => {
+  const { userName, password } = req.body;
+  knex("users")
+    .where("userName", userName)
+    .then((data) => {
+      const passwordCheck = bcrypt.compareSync(password, data[0].password);
+      console.log(data.length);
+      if (data.length && passwordCheck) {
+        const payload = {
+          id: data[0].id,
+          userName: data[0].userName,
+          firstName: data[0].firstName,
+          lastName: data[0].lastName,
+          email: data[0].email,
+          issuedAt: Date.now(),
+        };
+        const token = jwt.sign(payload, "tempKey", { expiresIn: "3h" });
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({
+          message: "Invalid password",
+        });
+      }
+    })
+    .catch((error) => {
+      res
+        .status(401)
+        .json({ message: "Unable to find user with that name", error: error });
+    });
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
+const userController = require("../controllers/userController");
 
-router.route("/");
+router.route("/").get(userController.userList);
+router.route("/:id").get(userController.userDetails);
+router.route("/login").post(userController.login);
+router.route("/register").post(userController.register);
 
 module.exports = router;
