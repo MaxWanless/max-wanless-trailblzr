35e784c2d2935484ba7910d20dc04876f6731bbf
Max Wanless <maxwanless@gmail.com>
2022-09-20 20:46:39 -0400

    Fixed park details response formating

--- server/controllers/parksController.js
+++ server/controllers/parksController.js
@@ -1,9 +1,11 @@
+const { response } = require("express");
+
 const knex = require("knex")(require("../knexfile"));
 
 exports.parkList = (req, res) => {
   knex
     .raw(
-      "select parks.*,  count(parktrails.parkID) As NumberOfTrails From parktrails left join parks on parktrails.parkID = parks.id group by parks.id"
+      "select parks.*,  count(parktrails.parkID) As trailCount From parktrails left join parks on parktrails.parkID = parks.id group by parks.id"
     )
     .then((data) => {
       res.status(200).json(data[0]);
@@ -11,17 +13,25 @@ exports.parkList = (req, res) => {
 };
 
 exports.parkDetails = (req, res) => {
+  let ParkData = {};
   const sendResponse = (data) => {
     res.status(200).json(data);
   };
 
   knex("parks")
     .where({ id: req.params.id })
-    .then((data) => {
+    .then((parkInfo) => (ParkData = parkInfo[0]))
+    .then(() => {
       knex("parktrails")
         .where({ parkID: req.params.id })
-        .then((parkTrails) => {
-          sendResponse({ ...data, trails: parkTrails });
+        .then((parkTrails) => (ParkData["trails"] = parkTrails))
+        .then(() => {
+          knex("parkhighlights")
+            .where({ parkID: req.params.id })
+            .then((parkHighlights) => (ParkData["highlights"] = parkHighlights))
+            .then(() => {
+              sendResponse(ParkData);
+            });
         });
     });
 };
