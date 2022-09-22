7cd1dffb796bc8956ee6ec38d070778eda17de0b
Max Wanless <maxwanless@gmail.com>
2022-09-22 14:31:36 -0400

    Update parks controllers with new DB table info

--- server/controllers/parksController.js
+++ server/controllers/parksController.js
@@ -1,11 +1,9 @@
-const { response } = require("express");
-
 const knex = require("knex")(require("../knexfile"));
 
 exports.parkList = (req, res) => {
   knex
     .raw(
-      "select parks.*,  count(parktrails.parkID) As trailCount From parktrails left join parks on parktrails.parkID = parks.id group by parks.id"
+      "select park.*,  count(park_trails.parkID) As trailCount From park_trails left join park on park_trails.parkID = park.id group by park.id"
     )
     .then((data) => {
       res.status(200).json(data[0]);
@@ -14,23 +12,34 @@ exports.parkList = (req, res) => {
 
 exports.parkDetails = (req, res) => {
   let ParkData = {};
+
   const sendResponse = (data) => {
     res.status(200).json(data);
   };
 
-  knex("parks")
+  knex("park")
     .where({ id: req.params.id })
     .then((parkInfo) => (ParkData = parkInfo[0]))
     .then(() => {
-      knex("parktrails")
+      knex("park_info")
+        .select("phone", "address", "size", `established`, `social`)
         .where({ parkID: req.params.id })
-        .then((parkTrails) => (ParkData["trails"] = parkTrails))
+        .then((parkInfo) => (ParkData["info"] = parkInfo[0]))
         .then(() => {
-          knex("parkhighlights")
+          knex("park_trails")
+            .select("name", "length", "difficulty", `description`)
             .where({ parkID: req.params.id })
-            .then((parkHighlights) => (ParkData["highlights"] = parkHighlights))
+            .then((parkTrails) => (ParkData["trails"] = parkTrails))
             .then(() => {
-              sendResponse(ParkData);
+              knex("park_highlights")
+                .pluck("highlight")
+                .where({ parkID: req.params.id })
+                .then(
+                  (parkHighlights) => (ParkData["highlights"] = parkHighlights)
+                )
+                .then(() => {
+                  sendResponse(ParkData);
+                });
             });
         });
     });
