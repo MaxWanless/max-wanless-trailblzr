f37384fe8c367f7bbfb05dbd1ebf4da3459b5982
Max Wanless <maxwanless@gmail.com>
2022-09-22 10:09:19 -0400

    Refactored map to use google maps

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,50 +1,58 @@
 import { useState } from "react";
-import ReactMapGL from "react-map-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import React from "react";
+import mapStyles from "./mapStyles";
+import logo from "../../assets/logos/parks-logo.png";
+
+const mapContainerStyle = {
+  width: "100%",
+  height: "100%",
+};
+
+const center = {
+  lat: 50.429213,
+  lng: -85.857796,
+};
+
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
 
 const Map = () => {
-  const MAP_STYLES = {
-    light: "mapbox://styles/mapbox/light-v10",
-    dark: "mapbox://styles/mapbox/dark-v9",
-  };
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  });
 
-  const MAP_DEFAULT_OPTIONS = {
-    style: MAP_STYLES.light,
-  };
-
-  const MAP_INITIAL_VIEWSTATE = {
-    longitude: 20.762678,
-    latitude: -16.208708,
-    zoom: 10,
-  };
-  const [mapOptions, setMapOptions] = useState(MAP_DEFAULT_OPTIONS);
-  const [viewState, setViewState] = useState(MAP_INITIAL_VIEWSTATE);
-  const [mapRef, setMapRef] = useState([]);
-
-  const _setMapOptions = (options) => {
-    setMapOptions({
-      ...mapOptions,
-      ...options,
-    });
-  };
+  if (loadError) return <div>Load Error</div>;
+  if (!isLoaded) return <div>Loading</div>;
 
   return (
     <Card sx={{ height: "100%" }}>
       <CardContent sx={{ height: "100%" }}>
-        <ReactMapGL
-          mapStyle={mapOptions.style}
-          viewPort={{
-            longitude: 50.755287,
-            latitude:  -85.714065,
-            zoom: 1,
-          }}
-          mapboxApiAccessToken={
-            "pk.eyJ1IjoibWFib25nIiwiYSI6ImNrMm9qN2tiYTEwc3ozZG41emx6bW9uZnQifQ.PhojWq3UwsAlPB7LBvJiTw"
-          }
-        />
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={5}
+          center={center}
+          options={options}
+        >
+          <Marker
+            position={{ lat: 45.391486, lng: -79.214673 }}
+            icon={{
+              url: `${logo}`,
+              scaledSize: new window.google.maps.Size(30, 30),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+            }}
+          />
+        </GoogleMap>
       </CardContent>
     </Card>
   );
