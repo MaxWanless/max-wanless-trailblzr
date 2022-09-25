4960f59a24aed38bf875a210a7b1d1ea2e0d9f8e
Max Wanless <maxwanless@gmail.com>
2022-09-24 21:24:03 -0400

    Added Backend variables

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
+require("dotenv").config({ path: "./server/.env" });
 
 module.exports = {
   client: "mysql",
   connection: {
-    host: "127.0.0.1",
-    user: "root",
-    password: "rootroot",
-    database: "trailblzr",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     charset: "utf8",
   },
 };
